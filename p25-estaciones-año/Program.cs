7d2563387e69830596f605cefa2245baec1c40a6
Nagubi <36177490@uaz.edu.mx>
2023-02-23 10:37:49 -0600

    Actividad 5

---
+++ p25-estaciones-año/Program.cs
@@ -0,0 +1,10 @@
+﻿// Muestra la estación del año que corresponde al número introducido
+Console.Clear();
+Console.WriteLine("Muestra la estacion del año en base al número:\n");
+Console.Write("Dame un numero (1..4) ? ");
+int n = int.Parse(Console.ReadLine());
+if(n==1) Console.WriteLine("Primavera\n");
+if(n==2) Console.WriteLine("Verano\n");
+if(n==3) Console.WriteLine("Otoño\n");
+if(n==4) Console.WriteLine("Invierno\n");
+Console.WriteLine("\nProceso terminado ....");
---
+++ p26-tipo-angulo/Program.cs
@@ -0,0 +1,17 @@
+﻿// Muestra el tipo de angulo según los grados
+Console.Clear();
+Console.WriteLine("Muestra el tipo de angulo (0..360)\n");
+Console.Write("Dame un angulo ? ");
+int angulo = int.Parse(Console.ReadLine());
+if(angulo<90)
+Console.WriteLine("\nEs agudo");
+if(angulo==90)
+Console.WriteLine("\nEs recto");
+if(angulo>90 && angulo<180)
+Console.WriteLine("\nEs obtuso");
+if(angulo==180)
+Console.WriteLine("\nEs llano");
+if(angulo>180 && angulo<360)
+Console.WriteLine("\nEs concavo");
+if(angulo==360)
+Console.WriteLine("\nEs completo");
